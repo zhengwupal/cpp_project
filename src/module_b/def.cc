6ede440a431f525638a13a150b5ce5129d17599d
zhengwupal <zhengwupal@163.com>
2022-11-09 18:28:57 +0800

    update cmakelist

--- src/module_b/def.cc
+++ src/module_b/def.cc
@@ -2,9 +2,12 @@
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
 
 #include "abc.h"
 
+using json = nlohmann::json;
 void test2() {
   boost::posix_time::time_duration td(10, 21, 30);
   std::cout << td << std::endl;
@@ -12,4 +15,14 @@ void test2() {
   std::cout << std::endl;
 
   test1();
+
+  std::cout << std::endl;
+
+  json ex1 = json::parse(R"(
+  {
+    "pi": 3.141,
+    "happy": true
+  })");
+
+  std::cout << ex1.dump(4) << std::endl;
 }
